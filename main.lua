e673cf000b64cd440ffb24d20cb2c6106efb57a2
skeler <sergei@keler.net>
2017-11-01 19:03:55 +0300

    ds18b20

--- main.lua
+++ main.lua
@@ -1,17 +1,21 @@
-local ledPin = 4
+local ledPin = 2 -- 4 default
 local dhtPin = 1
-local tPin = 1
+local tPin = 3
 local iD = "TEST"
-local interval = 300000
-print("LED pin ",ledPin)
-local level=gpio.HIGH
+local interval = 60000
+
+ds18b20.setup(tPin)
 gpio.mode(ledPin, gpio.OUTPUT)
 local mytimer = tmr.create()
 mytimer:register(interval, tmr.ALARM_AUTO, function() 
-  gpio.write(ledPin, level)
-  level = level == gpio.HIGH and gpio.LOW or gpio.HIGH
+  gpio.write(ledPin, gpio.HIGH)
   -- DS18B20
-  ds18b20.setup(tPin)
+  local gTemp=-99
+  ds18b20.read(function(ind,rom,res,temp,tdec,par)
+    --print(ind,string.format("%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X",
+    --  string.match(rom,"(%d+):(%d+):(%d+):(%d+):(%d+):(%d+):(%d+):(%d+)")),res,temp,tdec,par)
+    gTemp=temp
+  end,{})
   -- DHT22
   status, temp, humi, temp_dec, humi_dec = dht.read(dhtPin)
   if status == dht.OK then
@@ -22,11 +26,11 @@ mytimer:register(interval, tmr.ALARM_AUTO, function()
           humi_dec
     ))
     http.get(string.format("http://dkamushkin.ru/cgi-bin/netmeg1.pl?i=%s&h=%d&t1=%d&t2=%d",
-      iD,math.floor(temp),math.floor(humi),0),
+      iD,math.floor(humi),math.floor(temp),gTemp),
       nil,nil)
   else
     print("Error reading dht22") 
   end
+  gpio.write(ledPin, gpio.LOW)
 end)
-mytimer:interval(interval) -- actually, 3 seconds is better!
 mytimer:start()
