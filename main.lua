ad40510c33007af9d5e88807344b9ce349c7f3d4
skeler <sergei@keler.net>
2017-11-01 19:36:57 +0300

    ds18b20

--- main.lua
+++ main.lua
@@ -2,9 +2,9 @@ local ledPin = 2 -- 4 default
 local dhtPin = 1
 local tPin = 3
 local iD = "TEST"
-local interval = 300000
+local interval = 10000
 
-gTemp=-99
+local gTemp=-99
 
 ds18b20.setup(tPin)
 gpio.mode(ledPin, gpio.OUTPUT)
@@ -12,28 +12,28 @@ local mytimer = tmr.create()
 mytimer:register(interval, tmr.ALARM_AUTO, function() 
   gpio.write(ledPin, gpio.HIGH)
   -- DS18B20
-  -- gTemp=-99
+  gTemp=-99
   ds18b20.read(function(ind,rom,res,temp,tdec,par)
     --print(ind,string.format("%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X",
     --  string.match(rom,"(%d+):(%d+):(%d+):(%d+):(%d+):(%d+):(%d+):(%d+)")),res,temp,tdec,par)
     gTemp=temp
-    print("DS18B20="..gTemp)
-  end,{})
-  -- DHT22
-  status, temp, humi, temp_dec, humi_dec = dht.read(dhtPin)
-  if status == dht.OK then
-    print(string.format("DHT Temperature:%d.%03d;Humidity:%d.%03d\r\n",
+    print("IN DS18B20="..gTemp)
+    -- DHT22
+    local status, temp, humi, temp_dec, humi_dec = dht.read(dhtPin)
+    if status == dht.OK then
+      print(string.format("DHT Temperature:%d.%03d;Humidity:%d.%03d\r\n",
           math.floor(temp),
           temp_dec,
           math.floor(humi),
           humi_dec
-    ))
-    http.get(string.format("http://dkamushkin.ru/cgi-bin/netmeg1.pl?i=%s&h=%d&t1=%d&t2=%d",
-      iD,math.floor(humi),math.floor(temp),gTemp),
-      nil,nil)
-  else
-    print("Error reading dht22") 
-  end
-  gpio.write(ledPin, gpio.LOW)
+      ))
+      http.get(string.format("http://dkamushkin.ru/cgi-bin/netmeg1.pl?i=%s&h=%d&t1=%d&t2=%d",
+        iD,math.floor(humi),math.floor(temp),gTemp),
+        nil,nil)
+    else
+      print("Error reading dht22") 
+    end
+    gpio.write(ledPin, gpio.LOW)
+  end,{})
 end)
 mytimer:start()
