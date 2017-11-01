ed2428ee06979ef7d9f0fd66b2be4ec1cf2d23c1
skeler <sergei@keler.net>
2017-11-01 18:18:13 +0300

    good

--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
 local ledPin = 4
-local dhtPin = 0
+local dhtPin = 1
 local tPin = 1
+local iD = "TEST"
+local interval = 300000
 print("LED pin ",ledPin)
 local level=gpio.HIGH
 gpio.mode(ledPin, gpio.OUTPUT)
 local mytimer = tmr.create()
-mytimer:register(10000, tmr.ALARM_AUTO, function() 
+mytimer:register(interval, tmr.ALARM_AUTO, function() 
   gpio.write(ledPin, level)
   level = level == gpio.HIGH and gpio.LOW or gpio.HIGH
   -- DS18B20
@@ -19,9 +21,12 @@ mytimer:register(10000, tmr.ALARM_AUTO, function()
           math.floor(humi),
           humi_dec
     ))
+    http.get(string.format("http://dkamushkin.ru/cgi-bin/netmeg1.pl?i=%s&h=%d&t1=%d&t2=%d",
+      iD,math.floor(temp),math.floor(humi),0),
+      nil,nil)
   else
     print("Error reading dht22") 
   end
 end)
-mytimer:interval(1000) -- actually, 3 seconds is better!
+mytimer:interval(interval) -- actually, 3 seconds is better!
 mytimer:start()
